ba38c8fde54ab723c9e68672ec81d32a5e8e7fb9
Carlos Brand <alejandrobrand8@gmail.com>
2021-02-20 13:28:49 -0800

    initial api routes setup

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+module.exports = router;
+
+router.get("/api/workouts",(req,res)=>{
+
+});
+
+router.put("/api/workouts/:id",(req,res)=>{
+
+});
+
+router.post("/api/workouts",(req,res)=>{
+
+});
+
+router.get("/api/workouts/range",(req,res)=>{
+  
+})
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public")); 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes.js"));
 
 
